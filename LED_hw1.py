f6993dffc1d62dfe1997f882ff1548f7324b4eac
teacher144123 <teacher144123@gmail.com>
2018-03-19 14:41:06 +0800

    build structure without thread

--- LED_hw1.py
+++ LED_hw1.py
@@ -1,5 +1,6 @@
 import RPi.GPIO as gpio
 import time
+from threading import Thread
 
 def setup():
     gpio.setmode(gpio.BCM)
@@ -7,29 +8,41 @@ def setup():
     gpio.setup(27, gpio.OUT)
     gpio.setup(22, gpio.OUT)
 
-def turnOnLED(gpio_num):
-    gpio.output(gpio_num, gpio.HIGH)
-def turnOffLED(gpio_num):
-    gpio.output(gpio_num, gpio.LOW)
 
-def redGreen(now):
-    if now == 0:
-        turnOnLED(22)
-    elif now == 15:
-        turnOffLED(22)
-        turnOnLED(27)
-    elif now == 23:
-        turnOffLED(27)
-        turnOnLED(17)
-    elif now == 28:
-        turnOffLED(17)
+light_num = {'red':17, 'yellow':22, 'green':27}
+def turnOnLED(light_name):
+    global light_num
+    gpio.output(light_num[light_name], gpio.HIGH)
+def turnOffLED(light_name):
+    gpio.output(light_num[light_name], gpio.LOW)
+
+
+def light_round(start):
+    light_setting = [['red',15], ['green',8], ['yellow',5]]
+    i = 0
+    while True:
+        light(light_setting[i])
+        i += 1
+        if i >= 3:
+            i = 0
+
+def light(light_sec):
+    if light_sec[0] == 'yellow':
+        for sec in range(light_sec[1]):     # light_sec[1] is the second
+            turnOnLED('yellow')
+            time.sleep(0.5)
+            turnOffLED('yellow')
+            time.sleep(0.5)
+    else:
+        turnOnLED(light_sec[0])
+        time.sleep(light_sec[1])
+        turnOffLED(light_sec[0])
+
 
 if __name__ == '__main__':
+    # setting GPIO
     setup()
 
-    for sec in range(0,30):
-        print(sec)
-        redGreen(sec)
-        time.sleep(0.5)
+    light_round('red')
 
     gpio.cleanup()
