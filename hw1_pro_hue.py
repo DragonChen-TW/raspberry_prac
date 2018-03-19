fc4218a3e5cff0d9074fe0657d5ca5a0e2fb1343
teacher144123 <teacher144123@gmail.com>
2018-03-20 00:27:50 +0800

    using get_array_of

--- hw1_pro_hue.py
+++ hw1_pro_hue.py
@@ -1,7 +1,9 @@
 from pydub import AudioSegment
 
 song = AudioSegment.from_mp3('./ocean.mp3')
+song = song.get_array_of_samples()
 
 print(song[:2000])
+
 for i in range(0,10000,100):
-    print(int(song.raw_data[i]))
+    print(song[i])
