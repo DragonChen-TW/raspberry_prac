62cc4e7ca542606b808789206771d04ddde04d13
teacher144123 <teacher144123@gmail.com>
2018-03-20 00:34:17 +0800

    draw plot

--- hw1_pro_hue.py
+++ hw1_pro_hue.py
@@ -1,4 +1,5 @@
 from pydub import AudioSegment
+import matplotlib.pyplot as plt
 
 song = AudioSegment.from_mp3('./ocean.mp3')
 song = song.get_array_of_samples()
@@ -7,3 +8,6 @@ print(song[:2000])
 
 for i in range(0,10000,100):
     print(song[i])
+
+plt.plot(song[:10000])
+plt.show()
