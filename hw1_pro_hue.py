5f3977e92c9466fd17ef49b1fc2dbafc4af788cc
teacher144123 <teacher144123@gmail.com>
2018-03-19 21:02:35 +0800

    fix for

--- hw1_pro_hue.py
+++ hw1_pro_hue.py
@@ -3,4 +3,5 @@ from pydub import AudioSegment
 song = AudioSegment.from_mp3('./ocean.mp3')
 
 print(song[:2000])
-print(song.raw_data[:2000], end=' ')
+for i in range(0,10000,100):
+    print(song.raw_data[i])
