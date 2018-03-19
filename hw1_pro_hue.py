f892b512110b796496c985b6bc612ccba28719a3
teacher144123 <teacher144123@gmail.com>
2018-03-20 00:39:44 +0800

    setting seconds

--- hw1_pro_hue.py
+++ hw1_pro_hue.py
@@ -6,8 +6,10 @@ song = song.get_array_of_samples()
 
 print(song[:2000])
 
-for i in range(0,10000,100):
-    print(song[i])
+# for i in range(0,10000,100):
+#     print(song[i])
 
-plt.plot(song[:10000])
+sec = list(range(0,10000))
+
+plt.plot(sec, song[:10000])
 plt.show()
