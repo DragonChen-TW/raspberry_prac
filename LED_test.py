3092f19ff91b929ac1aa64393d0394985e4cedf1
teacher144123 <teacher144123@gmail.com>
2018-03-20 20:58:27 +0800

    fix gpio num

--- LED_test.py
+++ LED_test.py
@@ -3,7 +3,7 @@ import RPi.GPIO as gpio
 gpio.setmode(gpio.BCM)
 gpio.setup(17, gpio.OUT)
 
-p = gpio.PWM(12, 0.5)
+p = gpio.PWM(17, 0.5)
 p.start(1)
 input('Press return to stop:')   # use raw_input for Python 2
 p.stop()
