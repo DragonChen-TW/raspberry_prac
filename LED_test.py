847e5cd10938034b89bec6b1d9cd3bc15081d85b
teacher144123 <teacher144123@gmail.com>
2018-03-20 20:52:51 +0800

    add LED_test to PWM

---
+++ LED_test.py
@@ -0,0 +1,10 @@
+import RPi.GPIO as gpio
+
+gpio.setmode(gpio.BCM)
+gpio.setup(17, gpio.OUT)
+
+p = gpio.PWM(12, 0.5)
+p.start(1)
+input('Press return to stop:')   # use raw_input for Python 2
+p.stop()
+gpio.cleanup()
