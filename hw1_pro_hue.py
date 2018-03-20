a6b501e14242c5b667223a497062948782cc82f9
teacher144123 <teacher144123@gmail.com>
2018-03-20 23:17:23 +0800

    music with LED 0.1

--- hw1_pro_hue.py
+++ hw1_pro_hue.py
@@ -1,14 +1,27 @@
+# import RPi.GPIO as gpio
 from pydub import AudioSegment
 import numpy as np
-import matplotlib.pyplot as plt
 
-song = AudioSegment.from_mp3('mp3/lonely.mp3')
-song = song.set_frame_rate(192)
-song = song.get_array_of_samples()
-song = np.abs(song)
+global p
+def setup():
+    global p
+    gpio.setmode(gpio.BCM)
+    gpio.setup(17, gpio.OUT)
+    p = gpio.PWM(17, 100)  # why can I just setting freq=1
+    p.start(100)
 
-# for i in range(1,10000,200):
-#     print(i, song[i])
+def changeLED(signal):
+    global p
+    p.ChangeDutyCycle(signal)
 
-plt.plot(song[:9000])
-plt.show()
+if __name__ == '__main__':
+    # setup()
+
+    song = AudioSegment.from_mp3('mp3/lonely.mp3')
+    song = song.set_frame_rate(192)
+    song = song.get_array_of_samples()
+    song = np.abs(song)
+
+    for i in range(0,20000,100):
+        changeLED(i / 30000 * 100)
+        time.sleep(0.1)
