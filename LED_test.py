ad9c3df116e8e0ebfd97421f5bed56ae9b2551b5
teacher144123 <teacher144123@gmail.com>
2018-03-20 23:02:01 +0800

    PWM freq test

--- LED_test.py
+++ LED_test.py
@@ -5,16 +5,16 @@ gpio.setmode(gpio.BCM)
 gpio.setup(17, gpio.OUT)
 
 try:
-    p = gpio.PWM(17, 1)  # why can I just setting freq=1
+    p = gpio.PWM(17, 100)  # why can I just setting freq=1
     p.start(100)
 
     while True:
-        for i in range(0, 101, 5):
+        for i in range(0, 101, 1):
             p.ChangeDutyCycle(i)
-            time.sleep(0.1)
+            time.sleep(0.01)
         # time.sleep(1)
-        for i in range(100, -1, -5):
+        for i in range(100, -1, -1):
             p.ChangeDutyCycle(i)
-            time.sleep(0.1)
+            time.sleep(0.01)
 finally:
     gpio.cleanup()
