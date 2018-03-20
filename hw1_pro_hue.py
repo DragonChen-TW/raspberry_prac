508493f2b84c561e5359c03e8843343b069a87f4
teacher144123 <teacher144123@gmail.com>
2018-03-20 23:26:05 +0800

    test output sec

--- hw1_pro_hue.py
+++ hw1_pro_hue.py
@@ -16,14 +16,19 @@ def changeLED(signal):
     p.ChangeDutyCycle(signal)
 
 if __name__ == '__main__':
-    setup()
+    try:
+        setup()
 
-    song = AudioSegment.from_mp3('mp3/lonely.mp3')
-    song = song.set_frame_rate(192)
-    song = song.get_array_of_samples()
-    song = np.abs(song)
+        song = AudioSegment.from_mp3('mp3/lonely.mp3')
+        song = song.set_frame_rate(192)
+        song = song.get_array_of_samples()
+        song = np.abs(song)
 
-    for i in range(0,20000,100):
-        print(i, song[i] / 30000 * 100)
-        changeLED(song[i] / 30000 * 100)
-        time.sleep(0.1)
+        max_signal = 20000
+
+        for i in range(0,20000,100):
+            print(i, song[i] * 100 / max_signal)
+            changeLED(song[i] * 100 / max_signal)
+            time.sleep(0.1)
+    finally:
+        gpio.cleanup()
