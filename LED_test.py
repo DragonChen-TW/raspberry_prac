4f0f2634bb361b8d56700428afb33938ead17edd
teacher144123 <teacher144123@gmail.com>
2018-03-20 22:55:00 +0800

    test PWM keep show

--- LED_test.py
+++ LED_test.py
@@ -3,8 +3,8 @@ import RPi.GPIO as gpio
 gpio.setmode(gpio.BCM)
 gpio.setup(17, gpio.OUT)
 
-p = gpio.PWM(17, 100)
-p.start(99)
+p = gpio.PWM(17, 1)
+p.start(100)
 
 input('Press return to stop:')   # use raw_input for Python 2
 p.stop()
