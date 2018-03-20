caa51ffc2da6d84f6b9f40cacfa31041942f9e8c
teacher144123 <teacher144123@gmail.com>
2018-03-20 22:57:40 +0800

    test PWM keep show

--- LED_test.py
+++ LED_test.py
@@ -3,9 +3,15 @@ import RPi.GPIO as gpio
 gpio.setmode(gpio.BCM)
 gpio.setup(17, gpio.OUT)
 
-p = gpio.PWM(17, 1)
-p.start(100)
+try:
+    p = gpio.PWM(17, 1)  # why can I just setting freq=1
+    p.start(100)
 
-input('Press return to stop:')   # use raw_input for Python 2
-p.stop()
-gpio.cleanup()
+    while True:
+        for i in range(0, 101, 1):
+            p.ChangeDutyCycle(i)
+        for i in range(100, -1, -1):
+            p.ChangeDutyCycle(i)
+        time.sleep(1)
+finally:
+    gpio.cleanup()
