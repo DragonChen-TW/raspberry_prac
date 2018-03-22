a27d2e7217ed7ec68cc622636ff74e7bbe4bc9aa
Jackson Chen <teacher144123@gmail.com>
2018-03-22 20:01:32 +0800

    add hw2/PIR_1.py

---
+++ hw2/PIR_1.py
@@ -0,0 +1,25 @@
+import RPi.GPIO as gpio
+import time
+def setup(gpio_num):
+    gpio.setmode(gpio.BCM)
+    gpio.setup(gpio_num, gpio.OUT, pull_up_down=gpio.PUD_DOWN)
+
+count = 0
+def motion(gpio_num):
+    global count
+
+    if gpio.input(gpio_num):
+        count += 1
+        print(f'Motion detected {count} times.')
+    else:
+        print('Motion not detected.')
+
+
+if __name__ == '__main__':
+    try:
+        setup(14)
+        gpio.add_event_detect(14, gpio.BOTH, callback=motion, bouncetime=200)
+        while True:
+            time.sleep(1)
+    finally:
+        gpio.cleanup()
