895ba8961e345193e4cd1cf010f8cefa69b3e7dc
teacher144123 <teacher144123@gmail.com>
2018-03-22 11:20:49 +0800

    std version 0.8

--- hw1_pro_hue.py
+++ hw1_pro_hue.py
@@ -21,35 +21,21 @@ if __name__ == '__main__':
         # setup
         setup()
 
+        # variable setting
+        SEC = 1000
+        CHUNK = 200
+        rms_list = []
+
         # get data from mp3
         song = AudioSegment.from_mp3('mp3/lonely.mp3')
-        song = song.get_array_of_samples()
-        song = np.abs(song)
+        song = song[0:70 * SEC]
 
-        song_clean = []
-        CHUNK = 1000
+        for i in range(CHUNK,len(song[:30000]) + CHUNK, CHUNK):
+            rms_list.append(song[i - CHUNK:i + CHUNK].rms)
 
-        for i in range(CHUNK * 2,7000000, CHUNK):
-            data = song[i - CHUNK:i + CHUNK]
-            peak = np.average(data)
-
-            if peak > 12000:
-                song_clean.append(12000)
-            else:
-                song_clean.append(peak)
-
-        print(len(song_clean))
-
-        import matplotlib.pyplot as plt
-        plt.plot(song_clean)
-        plt.show()
-
-        global p
-
-        for i in range(len(song_clean)):
-            print(i, song_clean[i] / 120)
-            changeLED(int(song_clean[i] / 120))
-            time.sleep(1 / 90)
+        for rms in rms_list:
+            changeLED(rms / 6000)
+            time.sleep(CHUNK / SEC)
     finally:
         gpio.cleanup()
         pass
--- mp3_test.py
+++ mp3_test.py
@@ -4,29 +4,36 @@ import numpy as np
 import time
 
 song = AudioSegment.from_mp3('mp3/lonely.mp3')
-# song = song.set_frame_rate(10)
+print(len(song))
+song = song.set_frame_rate(44100)
 song = song.get_array_of_samples()
 song = np.abs(song)
+
+print(len(song))
+
+plt.plot(song)
+plt.show()
+
 
 
 song_clean = []
 CHUNK = 1000
 
-for i in range(CHUNK,len(song), CHUNK):
-    data = song[i - CHUNK:i + CHUNK]
-    peak = np.average(data)
-
-    if peak > 12000:
-        song_clean.append(12000)
-    else:
-        song_clean.append(peak)
-    # print("%04d %05d %s"%(i,peak,bars))
-    # time.sleep(0.1)
-
-print(len(song))
-print(len(song_clean))
-
-plt.plot(song)
-plt.show()
-plt.plot(song_clean)
-plt.show()
+# for i in range(CHUNK,len(song), CHUNK):
+#     data = song[i - CHUNK:i + CHUNK]
+#     peak = np.average(data)
+#
+#     if peak > 12000:
+#         song_clean.append(12000)
+#     else:
+#         song_clean.append(peak)
+#     # print("%04d %05d %s"%(i,peak,bars))
+#     # time.sleep(0.1)
+#
+# print(len(song))
+# print(len(song_clean))
+#
+# plt.plot(song)
+# plt.show()
+# plt.plot(song_clean)
+# plt.show()
