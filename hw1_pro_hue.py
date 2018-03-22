3baa1c81c05a704ec8de6c63f12ca15570ff7656
teacher144123 <teacher144123@gmail.com>
2018-03-22 11:35:15 +0800

    0.92 change CHUNK size

--- hw1_pro_hue.py
+++ hw1_pro_hue.py
@@ -31,8 +31,8 @@ if __name__ == '__main__':
         song = AudioSegment.from_mp3('mp3/lonely.mp3')
         song = song[0:70 * SEC]
 
-        for i in range(CHUNK,len(song) + CHUNK, CHUNK):
-            rms.append(song[i - CHUNK:i + CHUNK].rms)
+        for i in range(CHUNK,len(song) + CHUNK*0.3, CHUNK):
+            rms.append(song[i - CHUNK:i + CHUNK*0.3].rms)
 
         for i in range(len(rms)):
             signal = rms[i] / 6600 * 100
