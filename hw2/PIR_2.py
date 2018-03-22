5d11c23a764dcaf7c32f0e2dc7b9372bb08d9e7f
Jackson Chen <teacher144123@gmail.com>
2018-03-22 20:26:39 +0800

    add hw2/PIR_2.py

---
+++ hw2/PIR_2.py
@@ -0,0 +1,36 @@
+import RPi.GPIO as gpio
+import time
+
+def setup(gpio_num):
+    gpio.setmode(gpio.BCM)
+    if OUT_IN == 'IN':
+        gpio.setup(gpio_num, gpio.IN, pull_up_down=gpio.PUD_DOWN)
+    elif OUT_IN == 'OUT':
+        gpio.setup(gpio_num, gpio.OUT)
+
+count = 0
+def motion(gpio_num):
+    global count
+
+    if gpio.input(gpio_num):
+        count += 1
+        print('Motion detected {} times.'.format(count))
+    # else:
+    #     print('Motion not detected.')
+
+def LED_blink(gpio_num, times):
+    for i in range(times):
+        gpio.output(gpio_num, gpio.HIGH)
+        time.sleep(0.5)
+        gpio.output(gpio_num, gpio.LOW)
+        time.sleep(0.5)
+
+if __name__ == '__main__':
+    try:
+        setup(15, 'IN')
+        setup(16, 'OUT')
+        gpio.add_event_detect(15, gpio.RISING, callback=motion, bouncetime=300)
+        while True:
+            time.sleep(1)
+    finally:
+        gpio.cleanup()
