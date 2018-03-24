c0437b27eb7ec4bccc7f134213d1c1805e5995ed
teacher144123 <teacher144123@gmail.com>
2018-03-25 01:07:54 +0800

    add finally

--- hw2/hw2pro_LEDplayed.py
+++ hw2/hw2pro_LEDplayed.py
@@ -1,4 +1,7 @@
 import hw2pro_light as light
 
 if __name__ == '__main__':
-    light.setup({'red':16, 'yellow':20, 'green':21})
+    try:
+        light.setup({'red':16, 'yellow':20, 'green':21})
+    finally:
+        pass
