f9ee467864501b242ee5cf220cf289fd6c7539d6
teacher144123 <teacher144123@gmail.com>
2018-03-25 01:21:18 +0800

    fix callback

--- hw2/hw2pro_LEDplayer.py
+++ hw2/hw2pro_LEDplayer.py
@@ -11,7 +11,7 @@ if __name__ == '__main__':
         lights = {'red':16, 'yellow':20, 'green':21}
         gpio_PIR = 14
         light.setup(lights, gpio_PIR)
-        gpio.add_event_detect(gpio_PIR, gpio.RISING, callback=motion, bouncetime=300)
+        gpio.add_event_detect(gpio_PIR, gpio.RISING, callback=trigger, bouncetime=300)
 
         while True:
             time.sleep(1)
