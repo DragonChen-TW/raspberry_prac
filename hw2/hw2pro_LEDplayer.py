d65e42077db87270f728dc474bd89f925eb408b6
teacher144123 <teacher144123@gmail.com>
2018-03-26 00:13:01 +0800

    add nextSong/play and pause

--- hw2/hw2pro_LEDplayer.py
+++ hw2/hw2pro_LEDplayer.py
@@ -40,6 +40,8 @@ def nextSong():
     light.turnOFF(lights['green'])
     light.blink(lights, 3)
     audio.nextSong()
+    audio.play()
+    audio.stop()
     light.turnON(lights['yellow'])
     status = 2
 
