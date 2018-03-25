d7c8439f875ce5fb63c4669cfba6e9a8867d5651
teacher144123 <teacher144123@gmail.com>
2018-03-25 23:53:48 +0800

    change order of func

--- hw2/hw2pro_LEDplayer.py
+++ hw2/hw2pro_LEDplayer.py
@@ -4,45 +4,6 @@ import time
 import hw2pro_light as light
 import hw2pro_audio as audio
 
-def trigger(gpio_num):
-    global lights, status, count
-    print('trigger status={}'.format(status))
-
-    if status == 1:
-        play()
-    elif status == 2:
-        count += 1
-    elif status == 3:
-        unpause()
-
-if __name__ == '__main__':
-    try:
-        lights = {'red':16, 'yellow':20, 'green':21}
-        gpio_PIR = 14
-        light.setup(lights, gpio_PIR)
-        audio.setup()
-        gpio.add_event_detect(gpio_PIR, gpio.RISING, callback=trigger, bouncetime=1)
-
-        status = 1
-        count = 0
-        i = 0
-
-        while True:
-            time.sleep(10)
-            print('{} times, count is {}'.format(i,count))
-            i += 1
-            if status == 2:
-                if count == 1:
-                    pause()
-                elif count == 2:
-                    stop()
-                elif count >= 3:
-                    nextSong()
-                count = 0
-    finally:
-        gpio.cleanup()
-
-
 # ============ Status ============
 # 1
 def stop():
@@ -83,3 +44,41 @@ def nextSong():
     status = 2
 
 # ============ Status ============
+
+def trigger(gpio_num):
+    global lights, status, count
+    print('trigger status={}'.format(status))
+
+    if status == 1:
+        play()
+    elif status == 2:
+        count += 1
+    elif status == 3:
+        unpause()
+
+if __name__ == '__main__':
+    try:
+        lights = {'red':16, 'yellow':20, 'green':21}
+        gpio_PIR = 14
+        light.setup(lights, gpio_PIR)
+        audio.setup()
+        gpio.add_event_detect(gpio_PIR, gpio.RISING, callback=trigger, bouncetime=1)
+
+        status = 1
+        count = 0
+        i = 0
+
+        while True:
+            time.sleep(10)
+            print('{} times, count is {}'.format(i,count))
+            i += 1
+            if status == 2:
+                if count == 1:
+                    pause()
+                elif count == 2:
+                    stop()
+                elif count >= 3:
+                    nextSong()
+                count = 0
+    finally:
+        gpio.cleanup()
