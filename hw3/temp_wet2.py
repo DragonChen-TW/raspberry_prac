fd7c7b629f7ecf882c66240b01ab29a0d9bb2645
Jackson Chen <teacher144123@gmail.com>
2018-03-29 21:02:14 +0800

    add hw3/temp_wet2.py

--- hw3/temp_wet2.py
+++ hw3/temp_wet2.py
@@ -1,5 +1,14 @@
 import Adafruit_DHT
-import time, LED
+import time, math, LED
+
+def heatStroke(temp, wet):
+    return temp + wet * 0.1
+
+def dewPoint(temp, wet):
+    a = 17.271, b = 237.7
+    d = (a * temp) / (b + temp) + math.log(wet / 100)
+    return (b * temp) / (a - d)
+
 
 if __name__ == '__main__':
     sensor = Adafruit_DHT.DHT11
@@ -15,9 +24,12 @@ if __name__ == '__main__':
     while True:
         cur_time = time.ctime()
         wet, temp = Adafruit_DHT.read_retry(sensor, sensor_num)
+        dew_point = dewPoint(temp, wet)
+        heat_stroke = heatStroke(temp, wet)
 
         if wet and temp:
             print('{} >> wet = {}, temp = {}'.format(cur_time, wet, temp))
+            print('heatStroke = {}, dewPoint = {}'.format(heat_stroke, dew_point))
         else:
             print('Fail to get reading!')
         time.sleep(5)
