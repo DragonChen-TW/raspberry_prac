0ef3c9c54720cd181b52c169142c247d730a64e3
Jackson Chen <teacher144123@gmail.com>
2018-04-12 17:08:09 +0800

    edit doremi freq

--- hw3/doremi.py
+++ hw3/doremi.py
@@ -4,8 +4,8 @@ import RPi.GPIO as GPIO
 def doReMi():
     GPIO.setmode(GPIO.BCM)
     GPIO.setup(12, GPIO.OUT)
-    p = GPIO.PWM(12, 50)
-    p.start(0)
+    p = GPIO.PWM(12, 0.5)
+    p.start(1)
 
     print("Do")
     p.ChangeFrequency(523)
