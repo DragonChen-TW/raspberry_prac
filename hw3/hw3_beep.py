33deaab6899beffdb52b7c0b036af1a258305fd1
Jackson Chen <teacher144123@gmail.com>
2018-04-12 17:13:12 +0800

    edit beep problem

--- hw3/hw3_beep.py
+++ hw3/hw3_beep.py
@@ -2,8 +2,11 @@ import RPi.GPIO as gpio
 import time
 
 if __name__ == '__main__':
+    beep_num = 12
+    gpio.setmode(gpio.BCM)
+    gpio.setup(beep_num, gpio.OUT)
     time.sleep(2)
-    beep=gpio.PWM(12,800)
-    pwm_buzzer.satrt(0)
-    pwm_buzzer.ChangeDutyCycle(50)
-    pwm_buzzer.stop()
+    beep = gpio.PWM(beep_num,800)
+    beep.satrt(0)
+    beep.ChangeDutyCycle(50)
+    beep.stop()
