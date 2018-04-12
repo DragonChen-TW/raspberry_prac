aa7c9563246151959e9684f587431b383081e3b0
Jackson Chen <teacher144123@gmail.com>
2018-04-12 17:27:38 +0800

    add threading beep

--- hw3/hw3_environment.py
+++ hw3/hw3_environment.py
@@ -1,6 +1,7 @@
 import Adafruit_DHT
 import time
 import RPi.GPIO as gpio
+import threading as th
 import hw3_light as light
 import hw3_beep as beep
 
@@ -21,13 +22,15 @@ if __name__ == '__main__':
                 light.turnOFFALL(lights)
                 if temp >= 31:
                     light.turnON(lights['red'])
-                    beep.beepTimes(10)
+                    beep_th = beep_th.Thread(target=beep.bbeepTimes, args=(10,))
+                    beep_th.start()
                 elif temp >= 27:
                     light.turnON(lights['yellow'])
                 else:
                     light.turnON(lights['green'])
             else:
                 print('Fail to get reading!')
+            beep_th.join()
             time.sleep(5)
     finally:
         light.end()
