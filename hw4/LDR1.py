142aee0e29ae98087eb89d5d2bce202e857b498c
Jackson Chen <teacher144123@gmail.com>
2018-04-12 19:51:30 +0800

    edit LDR gpio_num

--- hw4/LDR1.py
+++ hw4/LDR1.py
@@ -3,7 +3,7 @@ import time, LED
 
 def setup():
     gpio.setmode(gpio.BCM)
-    gpio.setup(2, gpio.IN)
+    gpio.setup(14, gpio.IN)
     gpio.setup(16, gpio.OUT)
 
 def turnOnOff(gpio_num, LDR):
@@ -15,6 +15,6 @@ def turnOnOff(gpio_num, LDR):
 if __name__ == '__main__':
     setup()
     while True:
-        turnOnOff(16, gpio.input(2))
-        print(gpio.input(2))
+        turnOnOff(16, gpio.input(14))
+        print(gpio.input(14))
         time.sleep(1)
