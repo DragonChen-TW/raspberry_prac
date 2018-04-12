adab1aaacbc5e77c6606efb9f8d466b65467d30e
Jackson Chen <teacher144123@gmail.com>
2018-04-12 16:50:11 +0800

    add doremi test

---
+++ hw3/doremi.py
@@ -0,0 +1,25 @@
+import time
+import RPi.GPIO as GPIO
+
+def doReMi():
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setup(12, GPIO.OUT)
+    p = GPIO.PWM(12, 50)
+    p.start(50)
+
+    print("Do")
+    p.ChangeFrequency(523)
+    time.sleep(1)
+
+    print("Re")
+    p.ChangeFrequency(587)
+    time.sleep(1)
+
+    print("Mi")
+    p.ChangeFrequency(659)
+    time.sleep(1)
+
+    p.stop()
+    GPIO.cleanup()
+
+doReMi()
