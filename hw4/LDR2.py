10ceddf9f1d069d73cffba93aa419266db2d8343
Jackson Chen <teacher144123@gmail.com>
2018-04-12 20:39:32 +0800

    add LDR2.py

---
+++ hw4/LDR2.py
@@ -0,0 +1,25 @@
+import spidev
+import time
+
+def readadc(adc_num):
+    if adc_num > 7 or adc_num < 0:
+        return -1
+    global spi
+    r = spi.xfer2([1, 8 + adc_num << 4, 0])
+    data = ((r[1] & 3) << 8) + r[2]
+    return data
+
+if __name__ == '__main__':
+    # Global Variable
+    delay = 2
+    LDR_channel = 0
+
+    spi = spidev.SpiDev()
+    spi.open(0, 0)
+    spi.max_speed_hz = 1000000
+
+    while True:
+        LDR_value = readadc(LDR_channel)
+        print('-------------------')
+        print("LDR value is",LDR_value)
+        time.sleep(delay)
