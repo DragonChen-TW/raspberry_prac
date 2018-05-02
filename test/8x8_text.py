2184e1cbb18451777aa2c4379ce2a9cdd4719db4
Jackson Chen <teacher144123@gmail.com>
2018-05-03 00:33:48 +0800

    show image

--- test/8x8_text.py
+++ test/8x8_text.py
@@ -22,12 +22,12 @@ def show8x8(image, sec):
         for i in range(len(image)):
             for j in range(len(image[i])):
                 if image[i][j]:
-                    gpio.output(row_led[i], gpio.HIGH)
+                    gpio.output(row_led[j], gpio.HIGH)
                 else:
-                    gpio.output(row_led[i], gpio.LOW)
-            gpio.output(col_led[j], gpio.LOW)
+                    gpio.output(row_led[j], gpio.LOW)
+            gpio.output(col_led[i], gpio.LOW)
             time.sleep(0.001)
-            gpio.output(col_led[j], gpio.HIGH)
+            gpio.output(col_led[i], gpio.HIGH)
 
 
 col_led = [17, 27, 22, 5, 6, 13, 19, 26]
