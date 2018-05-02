29d1d131daf8cc4f5210e7bd9743f585050b7d3f
Jackson Chen <teacher144123@gmail.com>
2018-05-02 20:56:36 +0800

    add LED_test.py

---
+++ test/8x8_text.py
@@ -0,0 +1,12 @@
+import RPi.GPIO as gpio
+
+def setupAll():
+    global col_led, row_led
+    for i in col_led + row_led:
+        gpio.setup(i, gpio.OUT)
+
+col_led = [17, 27, 22, 5, 6, 13, 19, 26]
+row_led = [14, 15, 18, 23, 12, 16, 20, 21]
+
+if __name__ == '__main__':
+    setupAll()
