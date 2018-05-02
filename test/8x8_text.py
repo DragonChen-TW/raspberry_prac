dd90483d3336d9d715d89322dbb751fa55184f30
Jackson Chen <teacher144123@gmail.com>
2018-05-02 21:05:12 +0800

    edit LED_test.py

--- test/8x8_text.py
+++ test/8x8_text.py
@@ -5,7 +5,8 @@ def setupAll():
     gpio.setmode(gpio.BCM)
     global col_led, row_led
     for i in col_led + row_led:
-        gpio.setup(i, gpio.OUT)
+        try:
+            gpio.setup(i, gpio.OUT)
 
 col_led = [17, 27, 22, 5, 6, 13, 19, 26]
 row_led = [14, 15, 18, 23, 12, 16, 20, 21]
@@ -13,7 +14,7 @@ row_led = [14, 15, 18, 23, 12, 16, 20, 21]
 if __name__ == '__main__':
     try:
         setupAll()
-        
+
         time.sleep(10)
         for i in col_led:
             gpio.output(i, gpio.LOW)
