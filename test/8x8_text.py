92d2af96925dda49948a2b42f8e55404fc471ba2
Jackson Chen <teacher144123@gmail.com>
2018-05-02 23:31:58 +0800

    edit gpio num

--- test/8x8_text.py
+++ test/8x8_text.py
@@ -11,7 +11,7 @@ def setupAll():
         except:
             print(i)
 col_led = [17, 27, 22, 5, 6, 13, 19, 26]
-row_led = [14, 15, 18, 23, 12, 16, 20, 21]
+row_led = [15, 18, 23, 24, 12, 16, 20, 21]
 
 if __name__ == '__main__':
     try:
