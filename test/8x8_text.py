0c6cb280a4ae152096145f71a76af8b2e27c0c0b
Jackson Chen <teacher144123@gmail.com>
2018-05-02 21:01:42 +0800

    edit LED_test.py

--- test/8x8_text.py
+++ test/8x8_text.py
@@ -1,6 +1,8 @@
 import RPi.GPIO as gpio
+import time
 
 def setupAll():
+    gpio.setmode(gpio.BCM)
     global col_led, row_led
     for i in col_led + row_led:
         gpio.setup(i, gpio.OUT)
@@ -11,5 +13,13 @@ row_led = [14, 15, 18, 23, 12, 16, 20, 21]
 if __name__ == '__main__':
     try:
         setupAll()
+        
+        time.sleep(10)
+        for i in col_led:
+            gpio.output(i, gpio.LOW)
+
+        time.sleep(10)
+        for i in col_led:
+            gpio.output(i, gpio.HIGH)
     except:
         gpio.cleanup()
