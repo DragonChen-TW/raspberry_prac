212654f362a37e48c78e0765cabc9ec30d5165f1
Jackson Chen <teacher144123@gmail.com>
2018-05-10 00:20:31 +0800

    add post

--- rasp_fb/fb/views.py
+++ rasp_fb/fb/views.py
@@ -1,9 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views import View
+import json
 
 # Create your views here.
-def index(request):
-    if request.method == 'GET':
+# def index(request):
+#     if request.method == 'GET':
+#         if request.GET.get('hub.mode') == 'subscribe' and request.GET.get('hub.challenge'):
+#             if request.GET.get('hub.verify_token') == 'Small_Dragon_TW':
+#                 return HttpResponse(request.GET.get('hub.challenge'), status=200)
+#             else:
+#                 return HttpResponse('Verify Fail', status=403)
+#         else:
+#             return HttpResponse('Hello, world', status=200)
+#     else:
+#         pass
+
+class index(View):
+    def get(request):
         if request.GET.get('hub.mode') == 'subscribe' and request.GET.get('hub.challenge'):
             if request.GET.get('hub.verify_token') == 'Small_Dragon_TW':
                 return HttpResponse(request.GET.get('hub.challenge'), status=200)
@@ -11,5 +25,6 @@ def index(request):
                 return HttpResponse('Verify Fail', status=403)
         else:
             return HttpResponse('Hello, world', status=200)
-    else:
-        pass
+    def post(request):
+        with open('out.js', w) as js:
+            js.write(request.body)
