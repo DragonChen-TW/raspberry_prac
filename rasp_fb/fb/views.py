7d04cd8c451d0e81a1e1731587e6f835b6452155
Jackson Chen <teacher144123@gmail.com>
2018-05-10 01:26:20 +0800

    get inner data

--- rasp_fb/fb/views.py
+++ rasp_fb/fb/views.py
@@ -31,8 +31,8 @@ class index(View):
     def post(self, request):
         data = json.loads(str(request.body, 'utf-8'))
         # print(json.dumps(data, indent=' '))
-        if 'message' in data:
-            for msg_event in data['messaging']:
+        if 'messaging' in data['entry']:
+            for msg_event in data['entry']['messaging']:
                 sender_id = msg_event['sender']['id']
                 # msg_event
                 if msg_event['message']:
