36d30556de1a03471f15aca578adf77f4611d0a7
Jackson Chen <teacher144123@gmail.com>
2018-05-10 00:10:47 +0800

    fix problem

--- rasp_fb/fb/views.py
+++ rasp_fb/fb/views.py
@@ -6,7 +6,7 @@ def index(request):
     if request.method == 'GET':
         if request.GET.get('hub.mode') == 'subscribe' and request.GET.get('hub.challenge'):
             if request.GET.get('hub.verify_token') == 'Small_Dragon_TW':
-                return HttpResponse(request.GET.get('hub.challenge', status=200))
+                return HttpResponse(request.GET.get('hub.challenge'), status=200)
             else:
                 return HttpResponse('Verify Fail', status=403)
         else:
