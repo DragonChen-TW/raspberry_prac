e81da1b8e0fbfa8d185af12a7159923740210dde
Jackson Chen <teacher144123@gmail.com>
2018-05-10 01:30:12 +0800

    print more

--- rasp_fb/fb/views.py
+++ rasp_fb/fb/views.py
@@ -30,7 +30,7 @@ class index(View):
 
     def post(self, request):
         data = json.loads(str(request.body, 'utf-8'))
-        print(json.dumps(data, indent='  '))
+        print(json.dumps(data))
         if 'messaging' in data['entry']:
             for msg_event in data['entry']['messaging']:
                 sender_id = msg_event['sender']['id']
