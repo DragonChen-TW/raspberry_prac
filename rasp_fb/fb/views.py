d7dab15f73b9a61a0314354572334f8df31f9b4d
Jackson Chen <teacher144123@gmail.com>
2018-05-10 00:09:09 +0800

    edit view to verify

--- rasp_fb/fb/views.py
+++ rasp_fb/fb/views.py
@@ -1,7 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render,
 from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
     if request.method == 'GET':
-        return HttpResponse('Hello, world')
+        if request.GET.get('hub.mode') == 'subscribe' and request.GET.get('hub.challenge'):
+            if request.GET.get('hub.verify_token') == 'Small_Dragon_TW':
+                return HttpResponse(request.GET.get('hub.challenge', status=200))
+            else:
+                return HttpResponse('Verify Fail', status=403)
+        else:
+            return HttpResponse('Hello, world', status=200)
+    else:
+        pass
