45217b01407f5f8d93af2d56d1e646a2c475c816
Jackson Chen <teacher144123@gmail.com>
2018-05-10 03:08:51 +0800

    fix

--- rasp_fb/fb/views.py
+++ rasp_fb/fb/views.py
@@ -5,19 +5,6 @@ from django.views import View
 import json
 
 # Create your views here.
-# def index(request):
-#     if request.method == 'GET':
-#         if request.GET.get('hub.mode') == 'subscribe' and request.GET.get('hub.challenge'):
-#             if request.GET.get('hub.verify_token') == 'Small_Dragon_TW':
-#                 return HttpResponse(request.GET.get('hub.challenge'), status=200)
-#             else:
-#                 return HttpResponse('Verify Fail', status=403)
-#         else:
-#             return HttpResponse('Hello, world', status=200)
-#     else:
-#         pass
-
-
 class index(View):
     def get(self, request):
         if request.GET.get('hub.mode') == 'subscribe' and request.GET.get('hub.challenge'):
@@ -34,7 +21,7 @@ class index(View):
         for entry in data['entry']:
             if 'messaging' in entry:
                 for msg in entry['messaging']:
-                    print(msg['sender'], end=' ')
+                    print(msg['sender']['id'], end=' ')
                     print(msg['message']['text'])
 
         return HttpResponse('okay', status=200)
