84fe6dac759852e00931bee63cb67779b57772b0
Jackson Chen <teacher144123@gmail.com>
2018-05-10 21:08:36 +0800

    add client

--- hw7/wifi_client.py
+++ hw7/wifi_client.py
@@ -1,8 +1,15 @@
 import socket
 
 def client():
-    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client_socket.connect('')
+    try:
+        client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        client_socket.connect(('192.168.10.33', 8888))
+
+        while True:
+            data = input()
+            client_socket.send(data)
+    finally:
+        client_socket.close()
 
 if __name__ == '__main__':
     client()
