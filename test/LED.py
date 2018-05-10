4c3d76db13076076940a3d72555caca1755d8266
Jackson Chen <teacher144123@gmail.com>
2018-05-10 19:06:48 +0800

    add btn test

---
+++ test/LED.py
@@ -0,0 +1,12 @@
+import RPi.GPIO as gpio
+
+def setup(gpio_num, ctr):
+    if ctr == 'in':
+        gpio.setup(gpio_num, gpio.IN)
+    elif ctr == 'out':
+        gpio.setup(gpio_num, gpio.OUT)
+
+def turnON(gpio_num):
+    gpio.output(gpio_num, gpio.HIGH)
+def turnOFF(gpio_num):
+    gpio.output(gpio_num, gpio.LOW)
