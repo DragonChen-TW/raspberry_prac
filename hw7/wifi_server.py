8613cc7b71e12d17199b69df64dd964dcc27cd0c
Jackson Chen <teacher144123@gmail.com>
2018-05-10 20:47:15 +0800

    add wifi

---
+++ hw7/wifi_server.py
@@ -0,0 +1,33 @@
+import RPi.GPIO as gpio
+import socket, LED
+
+def server():
+    bind_ip = '192.168.2.185'
+    bind_port = 8888
+
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    server_socket.bind((bind_ip, bind_port))
+    server_socket.listen(5)
+
+    print('Listening on {}'.format((bind_ip, bind_port)))
+
+    gpio.setmode(gpio.BCM)
+    LED.setup(21, 'out')
+
+    try:
+        conn_socket, addr = server_socket.accept()
+        print('Accept connection from {}'.format((addr[0], addr[1])))
+        while True:
+            data = conn_socket.recv(1024)
+            data = str(data, 'utf-8')
+            if data == 'turn on':
+                LED.turnON(21)
+            elif data == 'turn off':
+                LED.turnOFF(21)
+    finally:
+        conn_socket.close()
+        gpio.cleanup()
+
+if __name__ == '__main__':
+    server()
