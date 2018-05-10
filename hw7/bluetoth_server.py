12dfce8a0e118decdfa469554cffd9bc9870e1fa
Jackson Chen <teacher144123@gmail.com>
2018-05-10 19:52:31 +0800

    add hw7

---
+++ hw7/bluetoth_server.py
@@ -0,0 +1,30 @@
+import RPi.GPIO as gpio
+import bluetooth as bt
+import LED
+
+def server():
+    gpio.setmode(BCM)
+    LED.setup(21, 'out')
+
+    server_socket = bt.BluetoothSocket(RFCOMM)
+    server_socket.bind(('', 3))
+    server_socket.listen(1)
+
+    conn_socket, address = server_socket.accept()
+    try:
+        while True:
+            data = conn_socket.recv(1024)
+            print(data)
+            if data == 'turn on':
+                print('receive {}'.format(data))
+                LED.turnON(21)
+
+            send_data = input()
+            conn_socket.send(send_data)
+    finally:
+        conn_socket.close()
+        server_socket.close()
+        gpio.cleanup()
+
+if __name__ == '__main__':
+    server()
--- test/btn_text.py
+++ test/btn_text.py
@@ -1,8 +1,6 @@
 import RPi.GPIO as gpio
 import time, LED
 
-
-
 if __name__ == '__main__':
     try:
         gpio.setmode(gpio.BCM)
@@ -15,7 +13,7 @@ if __name__ == '__main__':
                 LED.turnON(21)
             else:
                 LED.turnOFF(21)
-                
+
             time.sleep(0.5)
     finally:
         gpio.clenaup()
