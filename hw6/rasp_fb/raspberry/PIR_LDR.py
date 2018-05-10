4306e10549c7593eb606044e7273e9c323f6e6d0
Jackson Chen <teacher144123@gmail.com>
2018-05-10 15:31:31 +0800

    add hw6/raspberry

---
+++ hw6/rasp_fb/raspberry/PIR_LDR.py
@@ -0,0 +1,28 @@
+import RPi.GPIO as gpio
+import LED, time
+
+def motion(gpio_num):
+    global count
+    if gpio.input(gpio_num):
+        if gpio.input(23): # if dark
+            count += 1
+            LED.turnON(2)
+            print('Motion detected {}.'.format(count))
+            print('LDR is {}'.format(gpio.input(23)))
+    else:
+        LED.turnOFF(2)
+        print('Motion not detected')
+
+if __name__ == '__main__':
+    try:
+        gpio.setmode(gpio.BCM)
+        LED.setup(2, 'out')
+        gpio.setup(14, gpio.IN, pull_up_down=gpio.PUD_DOWN) #PIR
+        gpio.setup(23, gpio.IN) # LDR
+
+        count = 0
+        gpio.add_event_detect(14, gpio.BOTH, callback=motion, bouncetime=150)
+        while True:
+            time.sleep(1)
+    finally:
+        gpio.cleanup()
