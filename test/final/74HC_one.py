df41e38a3c3b4aff1abff4384e4460f3c99d44d1
Jackson Chen <teacher144123@gmail.com>
2018-05-16 11:21:38 +0800

    add test/final/

---
+++ test/final/74HC_one.py
@@ -0,0 +1,13 @@
+import RPi.GPIO as GPIO
+
+def setup():
+    gpio.setmode(gpio.BCM)
+    gpio.setup(16, gpio.OUT)
+
+
+if __name__ =="__main__":
+    try:
+        setup()
+        gpio.output(16, gpio.HIGH)
+    finally:
+        gpio.cleanup()
