63dc8e286c11f394aaeb81cb19a8a9fb13a52909
Jackson Chen <teacher144123@gmail.com>
2018-05-16 21:24:57 +0800

    modify PIR_test

--- hw7/PIR_test.py
+++ hw7/PIR_test.py
@@ -1,8 +1,14 @@
 import PIR, time
 import RPi.GPIO as gpio
 
-if __name__ == '__main__':
+def test():
     PIR.setup(14)
     while True:
         print(gpio.input(14))
         time.sleep(1)
+
+if __name__ == '__main__':
+    try:
+        test()
+    finally:
+        PIR.clean()
