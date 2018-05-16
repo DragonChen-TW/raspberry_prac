1fece7dc12919b5e4919a2f1e24cdffdbc552aeb
Jackson Chen <teacher144123@gmail.com>
2018-05-16 21:26:02 +0800

    add sleep

--- hw7/mqtt_publisher.py
+++ hw7/mqtt_publisher.py
@@ -1,5 +1,5 @@
 import paho.mqtt.client as mqtt
-import PIR
+import PIR, time
 import RPi.GPIO as gpio
 
 def server():
@@ -10,6 +10,7 @@ def server():
         PR_status = gpio.input(14)
         if PR_status == 1:
             mqtt_server.publish("LED", 'turn on')
+        time.sleep(1)
 
 if __name__ == '__main__':
     try:
