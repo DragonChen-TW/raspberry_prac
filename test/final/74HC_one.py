0a6a5d0cff16922ac4f3119937283cfc72560380
Jackson Chen <teacher144123@gmail.com>
2018-05-16 13:21:53 +0800

    modify 74HC_one.py

--- test/final/74HC_one.py
+++ test/final/74HC_one.py
@@ -2,17 +2,31 @@ import RPi.GPIO as gpio
 import time
 
 def setup():
+    # global variables
+    DS = 17
+    SHCP = 27
+    STCP = 22
+
     gpio.setmode(gpio.BCM)
     gpio.setup(16, gpio.OUT)
 
+def makeTick(gpio_num):
+    gpio.output(gpio_num, gpio.HIGH)
+    gpio.output(gpio_num, gpio.LOW)
+
+def shift(shift_data):
+    for i in shift_data:
+        gpio.output(SHCP, i)
+        makeTick(SHCP)
+
+def hc_out(data):
+    makeTick(SHCP)
+    shift(data)
+    makeTick(STCP)
 
 if __name__ =="__main__":
     try:
         setup()
-        gpio.output(16, gpio.HIGH)
-
-        while True:
-            time.sleep(1)
-
+        hc_out([1, 1, 1, 1, 0, 0, 0, 0])
     finally:
         gpio.cleanup()
