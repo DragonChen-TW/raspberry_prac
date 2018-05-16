2fae746c4f1d1a7ca9f124c0fe1efb4a05cb6536
Jackson Chen <teacher144123@gmail.com>
2018-05-16 11:24:13 +0800

    main loop

--- test/final/74HC_one.py
+++ test/final/74HC_one.py
@@ -1,4 +1,5 @@
 import RPi.GPIO as gpio
+import time
 
 def setup():
     gpio.setmode(gpio.BCM)
@@ -9,5 +10,9 @@ if __name__ =="__main__":
     try:
         setup()
         gpio.output(16, gpio.HIGH)
+
+        while True:
+            time.sleep(1)
+
     finally:
         gpio.cleanup()
