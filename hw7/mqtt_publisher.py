eea7e3169de44d48883163e1421849a37a1f3ba4
Jackson Chen <teacher144123@gmail.com>
2018-05-16 21:00:01 +0800

    modify PIR end

--- hw7/mqtt_publisher.py
+++ hw7/mqtt_publisher.py
@@ -15,5 +15,5 @@ if __name__ == '__main__':
     try:
         PR.setup(14)
         server()
-    except:
+    finally:
         PIR.clean()
--- hw7/mqtt_subscriber.py
+++ hw7/mqtt_subscriber.py
@@ -21,5 +21,5 @@ if __name__ == '__main__':
     try:
         LED.setup(21, 'out')
         client()
-    except:
+    finally:
         LED.clean()
