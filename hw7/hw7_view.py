f735f5a5af9d3b1ee7f39a5940037eea12637d7a
Jackson Chen <teacher144123@gmail.com>
2018-05-16 22:43:28 +0800

    send binary data

--- hw7/hw7_view.py
+++ hw7/hw7_view.py
@@ -7,8 +7,8 @@ def onConnect(view, data, flags, rc):
     view.subscribe('goods')
 def onMessage(view, data, msg):
     print(msg.topic, msg.payload)
-    uni_str = str(msg.payload, 'utf-8')
-    server(uni_str)
+    # uni_str = str(msg.payload, 'utf-8')
+    server(msg)
 
 def mqttSetup():
     global view
