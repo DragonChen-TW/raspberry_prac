94e82e91b45b8f2a22b7487672d67bd3e2f578d8
Jackson Chen <teacher144123@gmail.com>
2018-05-31 14:31:24 +0800

    add servo_round

---
+++ hw9/servo_round.py
@@ -0,0 +1,30 @@
+import RPi.GPIO as gpio
+import time
+
+def setup():
+    gpio.setmode(gpio.BCM)
+    gpio.setup(3, gpio.OUT)
+
+def setAngle(angle):
+    global pwm
+    duty_cycle = 1/20 * angle + 3
+    pwm.ChangeDutyCycle(duty_cycle)
+    time.sleep(1)
+    pwm.ChangeDutyCycle(0)
+
+if __name__ == '__main__':
+    setup()
+
+    pwm = gpio.PWM(3, 50)
+    pwm.start(0)
+    gpio.output(3, gpio.HIGH)
+
+    setAngle(180)
+    setAngle(0)
+
+    setAngle(90)
+    time.sleep(1)
+
+
+    pwm.stop()
+    gpio.cleanup()
