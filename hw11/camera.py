257cae90d1571f35d6d5b33a2c22c11d59d18312
Jackson Chen <teacher144123@gmail.com>
2018-06-14 19:10:44 +0800

    add hw11

---
+++ hw11/camera.py
@@ -0,0 +1,14 @@
+import cv2
+
+cam = cv2.VideoCapture(0)
+
+while cam.isOpened():
+    res, frame = cap.read()
+
+    csv.imshow('Video streaming', frame)
+    if csv.waitKey(1) & 0xFF == ord('q'):
+        break
+
+print('Close streaming')
+cam.release()
+cv2.destroyAllWindows()
