a4ebd9b23ea4cc78b921335823d80fdab9b510d4
Jackson Chen <teacher144123@gmail.com>
2018-06-14 19:12:53 +0800

    fix format

--- hw11/camera.py
+++ hw11/camera.py
@@ -5,8 +5,8 @@ cam = cv2.VideoCapture(0)
 while cam.isOpened():
     res, frame = cam.read()
 
-    csv.imshow('Video streaming', frame)
-    if csv.waitKey(1) & 0xFF == ord('q'):
+    cv2.imshow('Video streaming', frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
 print('Close streaming')
